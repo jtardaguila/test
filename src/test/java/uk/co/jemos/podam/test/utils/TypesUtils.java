84ce484cdd5587a7c70d58911ebf498846d4e6b9
Jorge Tardaguila <jtardaguila@users.noreply.github.com>
2015-11-25 13:51:49 -0300

    Update TypesUtils.java

--- src/test/java/uk/co/jemos/podam/test/utils/TypesUtils.java
+++ src/test/java/uk/co/jemos/podam/test/utils/TypesUtils.java
@@ -4,6 +4,11 @@ import org.junit.Assert;
 
 import java.util.Calendar;
 import java.util.Date;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
 
 public class TypesUtils {
 
